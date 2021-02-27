da9328de0b308d61e4d82289c1ad933de515a837
Jenner Garcia <jenner.garcia@gmail.com>
2021-02-27 11:04:13 -0500

    update connection

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,4 @@ INSERT INTO burgers (burger_name) VALUES ("Galaxy Burger");
 INSERT INTO burgers (burger_name) VALUES ("Massive Attack Burger");
 INSERT INTO burgers (burger_name) VALUES ("Triple Doodle Burger");
 INSERT INTO burgers (burger_name) VALUES ("The Frankenstein");
-INSERT INTO burgers (burger_name) VALUES ("Everything but the Sink Burger");
-INSERT INTO burgers (burger_name) VALUES ("The Tricky Double Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Tricky Double Cheese");
